a60a1591f973a5d7e807bf9d93b6ba516d7eea05
AWAIS MAYO KING009 <rojxb268@gmail.com>
2025-12-26 23:22:52 +0500

    Refactor makeid function for improved flexibility

--- gen-id.js
+++ gen-id.js
@@ -1,12 +1,10 @@
-function makeid(num = 4) {
-  let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
-  }
-  return result;
+function makeid(length = 10) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
-module.exports = {makeid};
-
-
+module.exports = { makeid };
