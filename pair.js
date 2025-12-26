9fe91d67502b8e1a4e085a3b60a0fe0d5d044334
AWAIS MAYO KING009 <rojxb268@gmail.com>
2025-12-26 17:29:39 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -62,21 +62,21 @@ router.get('/', async (req, res) => {
 
                     const successMsg = {
                         text:
-                            `🚀 *ARSLAN-MD Session Created!*\n\n` +
+                            `🚀 *AWAIS-MD Session Created!*\n\n` +
                             `▸ *Never share* your session ID\n` +
                             `▸ Join our WhatsApp Channel\n` +
                             `▸ Report bugs on GitHub\n\n` +
-                            `_Powered by ARSLAN-MD\n\n` +
+                            `_Powered by AWAIS-MD\n\n` +
                             `🔗 *Useful Links:*\n` +
-                            `▸ GitHub: https://github.com/Arslan-MD/Arslan_MD\n` +
-                            `▸ https://whatsapp.com/channel/0029VarfjW04tRrmwfb8x306`,
+                            `▸ GitHub: https://github.com/awaismayoking009/AWAIS_MANO_CYBER_BTZ\n` +
+                            `▸ https://whatsapp.com/channel/0029VbBGXs5HbFV5AM43wi22`,
                         contextInfo: {
                             mentionedJid: [sock.user.id],
                             forwardingScore: 1000,
                             isForwarded: true,
                             forwardedNewsletterMessageInfo: {
-                                newsletterJid: "120363348739987203@newsletter",
-                                newsletterName: "ARSLAN-MD",
+                                newsletterJid: "120363390235338421@newsletter",
+                                newsletterName: "AWAIS_MANO_CYBER_BTZ",
                                 serverMessageId: 143
                             }
                         }
