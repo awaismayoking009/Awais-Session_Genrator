a99a86b1433471c5eaa211947d018ca39b0078a1
AWAIS MAYO KING009 <rojxb268@gmail.com>
2025-12-27 07:22:51 +0500

    Change server port from 3000 to 8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const pair = require('./pair');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 app.use('/code', pair);
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server Online on ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
